f375b12195c6f7ca512c8f4d0160b0d73793598f
MATTHEW LEBLANC <S331382184@OCDSB>
2018-10-02 14:30:32 -0400

    Progress

--- MusicMaker.py
+++ MusicMaker.py
@@ -30,10 +30,8 @@ Red        = (255, 0  , 0  )
 DarkRed    = (150, 0  , 0  )
 Blue       = (0  , 0  , 255)
 Grey       = (75 , 75 , 75 )
-LightGrey  = (113, 117, 112)
+LightGrey  = (125, 125, 125)
 DarkGrey   = (50 , 50 , 50 )
-
-circleColour = (255, 255, 255)
 
 #Variables
 
@@ -42,11 +40,16 @@ Surface    = pygame.display.set_mode((windowWidth, windowHeight))
 fadeScreen = pygame.Surface((windowWidth, windowHeight))
 pygame.display.set_caption('Music Maker')
 
+#Def Functions
+def WhiteKey(x, y, color):
+    pygame.draw.rect(Surface, color, (x, y, 100, 245))
+    pygame.draw.rect(Surface, Black, (x-3, y-3, 103, 248), 3)
+
 #Extra Notes
 #Piano Tiles Length: 1024 Pixels
 #Individual Octive Length: 341 Pixels
 #White Tile Length: 48 Pixels
-#0.323/1.563
+#0.323/1.563z
 
 while True: #Game Loop
     for event in pygame.event.get():
@@ -54,7 +57,12 @@ while True: #Game Loop
             pygame.quit()
             sys.exit()
 
-    pygame.draw.rect(Surface, DarkGrey, (0, 0, windowWidth, windowHeight), 0)
+    pygame.draw.rect(Surface, LightGrey, (0, 0, windowWidth, windowHeight), 0)
+
+    WhiteKey(windowWidth-100, windowHeight-245, White)
+
+    #High Octive
+    
 
     pygame.display.flip()
     fpsClock.tick(FPS)

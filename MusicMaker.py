71dc37ee754decdfb2594fedd4e567199c9bf98a
MATTHEW LEBLANC <s331382184@ocdsb>
2018-10-09 12:11:53 -0400

    White Key Tiles Made

--- MusicMaker.py
+++ MusicMaker.py
@@ -41,7 +41,26 @@ for x in range(0, 8):
 noteColorsWhite = [(255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255)]
 noteColorsBlack = [(0  , 0  , 0  ), (0  , 0  , 0  ), (0  , 0  , 0  ), (0  , 0  , 0  ), (0  , 0  , 0  ), (0  , 0  , 0  ), (0  , 0  , 0  )]
 noteButtonList  = ["", ";", "L", "K", "J", "H", "G", "F", "D", "S", "A"]
-noteButtonList2 = ["", "P", "O", "U", "Y", "T", "E", "W"]
+
+#Paino Tiles Lists
+pianoTilesA = []
+pianoTilesS = []
+pianoTilesD = []
+pianoTilesF = []
+pianoTilesG = []
+pianoTilesH = []
+pianoTilesJ = []
+pianoTilesK = []
+pianoTilesL = []
+pianoTilesSEMI = []
+
+pianoTilesW = []
+pianoTilesE = []
+pianoTilesT = []
+pianoTilesY = []
+pianoTilesU = []
+pianoTilesO = []
+pianoTilesP = []
 
 #Set up the window
 Surface    = pygame.display.set_mode((windowWidth, windowHeight))
@@ -56,6 +75,23 @@ def WhiteKey(x, y, color):
 def BlackKey(x, y, color):
     pygame.draw.rect(Surface, color, (x, y, 45, (249/2)+20))
     pygame.draw.rect(Surface, Black, (x, y-2, 45, (249/2)+22), 3)
+
+def DeleteTiles(tileList):
+    for t in tileList[:]:
+        if t[1]+t[2] <= 0:
+            tileList.remove(t)
+            break
+
+def MoveTiles(tileList):
+    for t in tileList[:]:
+        t[1] -= 8
+        if t[3] == True:
+            t[2] += 8
+
+def DrawTiles(tileList):
+    for t in tileList[:]:
+        pygame.draw.rect(Surface, Green, (t[0], t[1], 90, t[2]-2))
+        pygame.draw.rect(Surface, Black, (t[0]-2, t[1]-2, 92, t[2]), 3)
 
 #Text Functions
 def text_objects(Text, font, colour):
@@ -79,24 +115,34 @@ while True: #Game Loop
             #Notes Pressed
             if (event.key == K_a):
                 noteColorsWhite[0] = (0, 255, 0)
+                pianoTilesA.append([windowWidth-920, windowHeight-247, 0, True])
             if (event.key == K_s):
                 noteColorsWhite[1] = (0, 255, 0)
+                pianoTilesS.append([windowWidth-828, windowHeight-247, 0, True])
             if (event.key == K_d):
                 noteColorsWhite[2] = (0, 255, 0)
+                pianoTilesD.append([windowWidth-736, windowHeight-247, 0, True])
             if (event.key == K_f):
                 noteColorsWhite[3] = (0, 255, 0)
+                pianoTilesF.append([windowWidth-644, windowHeight-247, 0, True])
             if (event.key == K_g):
                 noteColorsWhite[4] = (0, 255, 0)
+                pianoTilesG.append([windowWidth-552, windowHeight-247, 0, True])
             if (event.key == K_h):
                 noteColorsWhite[5] = (0, 255, 0)
+                pianoTilesH.append([windowWidth-460, windowHeight-247, 0, True])
             if (event.key == K_j):
                 noteColorsWhite[6] = (0, 255, 0)
+                pianoTilesJ.append([windowWidth-368, windowHeight-247, 0, True])
             if (event.key == K_k):
                 noteColorsWhite[7] = (0, 255, 0)
+                pianoTilesK.append([windowWidth-276, windowHeight-247, 0, True])
             if (event.key == K_l):
                 noteColorsWhite[8] = (0, 255, 0)
+                pianoTilesL.append([windowWidth-184, windowHeight-247, 0, True])
             if (event.key == K_SEMICOLON):
                 noteColorsWhite[9] = (0, 255, 0)
+                pianoTilesSEMI.append([windowWidth-92, windowHeight-247, 0, True])
 
             if (event.key == K_w):
                 noteColorsBlack[0] = (0  , 150, 0  )
@@ -124,24 +170,34 @@ while True: #Game Loop
             #Notes Released
             if (event.key == K_a):
                 noteColorsWhite[0] = (255, 255, 255)
+                pianoTilesA[len(pianoTilesA)-1][3] = False
             if (event.key == K_s):
                 noteColorsWhite[1] = (255, 255, 255)
+                pianoTilesS[len(pianoTilesS)-1][3] = False
             if (event.key == K_d):
                 noteColorsWhite[2] = (255, 255, 255)
+                pianoTilesD[len(pianoTilesD)-1][3] = False
             if (event.key == K_f):
                 noteColorsWhite[3] = (255, 255, 255)
+                pianoTilesF[len(pianoTilesF)-1][3] = False
             if (event.key == K_g):
                 noteColorsWhite[4] = (255, 255, 255)
+                pianoTilesG[len(pianoTilesG)-1][3] = False
             if (event.key == K_h):
                 noteColorsWhite[5] = (255, 255, 255)
+                pianoTilesH[len(pianoTilesH)-1][3] = False
             if (event.key == K_j):
                 noteColorsWhite[6] = (255, 255, 255)
+                pianoTilesJ[len(pianoTilesJ)-1][3] = False
             if (event.key == K_k):
                 noteColorsWhite[7] = (255, 255, 255)
+                pianoTilesK[len(pianoTilesK)-1][3] = False
             if (event.key == K_l):
                 noteColorsWhite[8] = (255, 255, 255)
+                pianoTilesL[len(pianoTilesL)-1][3] = False
             if (event.key == K_SEMICOLON):
                 noteColorsWhite[9] = (255, 255, 255)
+                pianoTilesSEMI[len(pianoTilesSEMI)-1][3] = False
 
             if (event.key == K_w):
                 noteColorsBlack[0] = (0, 0, 0)
@@ -165,13 +221,69 @@ while True: #Game Loop
                 noteColorsBlack[6] = (0, 0, 0)
                 blackNoteKeys[0]   = (255, 255, 255)
 
-
     #Background
     pygame.draw.rect(Surface, LightGrey, (0, 0, windowWidth, windowHeight))
     pygame.draw.rect(Surface, Grey, (windowWidth-920, 0, 920, windowHeight))
     pygame.draw.line(Surface, Black, (windowWidth-922, windowHeight), (windowWidth-922, 0), 5)
 
-    #High Octive
+    #Delete Tiles
+    DeleteTiles(pianoTilesA)
+    DeleteTiles(pianoTilesS)
+    DeleteTiles(pianoTilesD)
+    DeleteTiles(pianoTilesF)
+    DeleteTiles(pianoTilesG)
+    DeleteTiles(pianoTilesH)
+    DeleteTiles(pianoTilesJ)
+    DeleteTiles(pianoTilesK)
+    DeleteTiles(pianoTilesL)
+    DeleteTiles(pianoTilesSEMI)
+    DeleteTiles(pianoTilesW)
+    DeleteTiles(pianoTilesE)
+    DeleteTiles(pianoTilesT)
+    DeleteTiles(pianoTilesY)
+    DeleteTiles(pianoTilesU)
+    DeleteTiles(pianoTilesO)
+    DeleteTiles(pianoTilesP)
+
+    #Move Tiles
+    MoveTiles(pianoTilesA)
+    MoveTiles(pianoTilesS)
+    MoveTiles(pianoTilesD)
+    MoveTiles(pianoTilesF)
+    MoveTiles(pianoTilesG)
+    MoveTiles(pianoTilesH)
+    MoveTiles(pianoTilesJ)
+    MoveTiles(pianoTilesK)
+    MoveTiles(pianoTilesL)
+    MoveTiles(pianoTilesSEMI)
+    MoveTiles(pianoTilesW)
+    MoveTiles(pianoTilesE)
+    MoveTiles(pianoTilesT)
+    MoveTiles(pianoTilesY)
+    MoveTiles(pianoTilesU)
+    MoveTiles(pianoTilesO)
+    MoveTiles(pianoTilesP)
+
+    #Draw Tiles
+    DrawTiles(pianoTilesA)
+    DrawTiles(pianoTilesS)
+    DrawTiles(pianoTilesD)
+    DrawTiles(pianoTilesF)
+    DrawTiles(pianoTilesG)
+    DrawTiles(pianoTilesH)
+    DrawTiles(pianoTilesJ)
+    DrawTiles(pianoTilesK)
+    DrawTiles(pianoTilesL)
+    DrawTiles(pianoTilesSEMI)
+    DrawTiles(pianoTilesW)
+    DrawTiles(pianoTilesE)
+    DrawTiles(pianoTilesT)
+    DrawTiles(pianoTilesY)
+    DrawTiles(pianoTilesU)
+    DrawTiles(pianoTilesO)
+    DrawTiles(pianoTilesP)
+
+    #Draw Piano
     WhiteKey(windowWidth-92 , windowHeight-247, noteColorsWhite[9])
     WhiteKey(windowWidth-184, windowHeight-247, noteColorsWhite[8])
     WhiteKey(windowWidth-276, windowHeight-247, noteColorsWhite[7])
@@ -196,7 +308,7 @@ while True: #Game Loop
         Text(noteButtonList[i], windowWidth-(92*i)+11, windowHeight-15, 20, Black)
         if i > 9:
             break
-
+    
     #Black Key Text
     Text("P", windowWidth-104, windowHeight-112, 15, blackNoteKeys[0])
     Text("O", windowWidth-196, windowHeight-112, 15, blackNoteKeys[1])
@@ -208,4 +320,3 @@ while True: #Game Loop
     
     pygame.display.flip()
     fpsClock.tick(FPS)
-    
